df264e3c0594805c548c641381fb4e45180c93af
OferKoren <oferkoren2@gmail.com>
2024-09-10 15:54:28 +0300

    Add limit ball size

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var gCurrSize = 100
 function onBallClick(elBall) {
+    if (gCurrSize === 400) gCurrSize = 50
     elBall.style.width = `${gCurrSize + 50}px`
     elBall.style.height = `${gCurrSize + 50}px`
-    elBall.innerText = gCurrSize
+    elBall.innerText = gCurrSize + 50
     gCurrSize += 50
 
 }
