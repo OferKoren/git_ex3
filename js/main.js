a6b667b699aed9f76b1d4242bd4031c79d092ff7
OferKoren <oferkoren2@gmail.com>
2024-09-10 16:45:00 +0300

    Randomize ball size increase

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var gCurrSize = 100
 function onBallClick(elBall) {
-    if (gCurrSize === 400) gCurrSize = 50
-    elBall.style.width = `${gCurrSize + 50}px`
-    elBall.style.height = `${gCurrSize + 50}px`
-    elBall.innerText = gCurrSize + 50
+    if (gCurrSize >= 400) gCurrSize = 50
+
+    var randIncre = getRandomInt(20, 61)
+
+    elBall.style.width = `${gCurrSize + randIncre}px`
+    elBall.style.height = `${gCurrSize + randIncre}px`
+    elBall.innerText = gCurrSize + randIncre
     gCurrSize += 50
 
 }
