9beff46ecf119e09f96f76373390efa77d3cad11
OferKoren <oferkoren2@gmail.com>
2024-09-10 15:40:40 +0300

    Update onBallClick()

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+var gCurrSize = 100
+function onBallClick(elBall) {
+    elBall.style.width = `${gCurrSize + 50}px`
+    elBall.style.height = `${gCurrSize + 50}px`
+    elBall.innerText = gCurrSize
+    gCurrSize += 50
 
-function onBallClick() {
-    console.log("ball clicked")
 }
