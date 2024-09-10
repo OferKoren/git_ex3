f3b11786c034bbe25f5c9176e424b2b0874bdaaf
OferKoren <oferkoren2@gmail.com>
2024-09-10 16:48:28 +0300

    Add ball random color1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 var gCurrSize = 100
 function onBallClick(elBall) {
-    if (gCurrSize >= 400) gCurrSize = 50
+
 
     var randIncre = getRandomInt(20, 61)
+    var randColor = getRandomColor()
 
+    if (gCurrSize + randIncre >= 400) {
+        gCurrSize = 100;
+        randIncre = 0;
+    }
     elBall.style.width = `${gCurrSize + randIncre}px`
     elBall.style.height = `${gCurrSize + randIncre}px`
+    elBall.style.backgroundColor = `${randColor}`
     elBall.innerText = gCurrSize + randIncre
+
     gCurrSize += 50
 
 }
