71082bba84c7f4ecfbffc733153d326402092ee5
OferKoren <oferkoren2@gmail.com>
2024-09-11 07:26:05 +0300

    Add third ball

--- js/main.js
+++ js/main.js
@@ -17,3 +17,30 @@ function onBallClick(elBall, maxDiameter) {
     gCurrSize += 50
 
 }
+function onSwitchBalls() {
+    const elBallOne = document.querySelector('.ball-one')
+    const elBallTwo = document.querySelector('.ball-two')
+
+    const tempColor1 = elBallOne.style.backgroundColor
+    const tempSize1 = elBallOne.style.width
+    const function1 = elBallOne.onclick
+
+    const tempColor2 = elBallTwo.style.backgroundColor
+    const tempSize2 = elBallTwo.style.width
+    const function2 = elBallTwo.onclick
+
+    elBallOne.style.width = tempSize2
+    elBallOne.style.height = tempSize2
+    elBallOne.style.backgroundColor = tempColor2
+    elBallOne.innerText = tempSize2.split("px")[0]
+    elBallOne.onclick = function2
+
+    elBallTwo.style.width = tempSize1
+    elBallTwo.style.height = tempSize1
+    elBallTwo.style.backgroundColor = tempColor1
+    elBallTwo.innerText = tempSize1.split("px")[0]
+    elBallTwo.onclick = function1
+
+
+
+}
